07848fd3b8abff71d08ede8705228d9f633b1356
vatslav <vatslav.startup@yandex.ru>
2013-10-06 23:56:54 +0400

    попрбовал кучу вариантов. учу регулярные выражения на более выском уровне

--- hostHandler.py
+++ hostHandler.py
@@ -1,14 +1,48 @@
 __author__ = 'vatslav'
+import re
+import pprint
 file = open('input.txt', "r")
+#hosts = [line for line in file]
+#output = []
+domainSeach = 'domain.tld'
+domainRepl = 'donemain.tld'
+mashinSeash = 'bar'
+mashinRepl = 'baz'
 
 
-class HostHandler:
+class HostHandler(object):
     def __init__(self, f):
         self.f=f
 
-        self.hosts = [line for line in self.f]
+        self.hosts = ''.join(line for line in self.f)
+        output = []
         #buf = set(line in self.f)
-        print(self.hosts,type(self.hosts))
+        #self.print()
+        self.handler()
+        #self.printer()
+
+    def printer(self):
+        print(self.hosts)
+
+    @staticmethod
+    def __fixDomain_(m):
+        return ''
 
 
-hostHandler = HostHandler(file)
+    def handler(self):
+        #p = re.compile(r'(\sbar[.]domain[.]tld)')
+        t = re.compile(r'(\s[bar.domain.tld]+)')
+        t = re.compile('(?P<ip>[\d+.\d+.\d+.\d+]+) | (?P<domain>\w+)')
+        result = re.finditer(t,self.hosts)
+        for math  in result:
+            print(math.group())
+        #self.hosts=re.sub(p,'\tbaz.donemain.tld', self.hosts)
+        #p = re.compile(r'\sbaz.doneain.tld .')
+        #self.hosts = p.sub()
+
+
+
+
+d = HostHandler(file)
+
+
---
+++ regEdu/edu1.py
@@ -0,0 +1,13 @@
+__author__ = 'django'
+import re
+
+log='''64 bytes from localhost.localdomain (127.0.0.1): icmp_req=1 ttl=64 time=0.033 ms
+64 bytes from localhost.localdomain (127.0.0.1): icmp_req=2 ttl=64 time=0.034 ms
+64 bytes from localhost.localdomain (127.0.0.1): icmp_req=3 ttl=64 time=0.031 ms
+64 bytes from localhost.localdomain (127.0.0.1): icmp_req=4 ttl=64 time=0.031 ms'''
+pattern = re.compile('icmp_req=[\d]+.*(time=[\d]+[.]?[\d]+ ms)')
+m = re.finditer(pattern, log)
+result = []
+for x in m:
+    result.append(x.groups())
+print(result)
