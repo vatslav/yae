f68a590ea931deb6962e4ea261a387050ff8a71c
vatslav <vatslav.startup@yandex.ru>
2013-10-08 14:08:19 +0400

    task3 решил. ищу нужный формат вывода для проверочного бота

--- task3 - nsswitch.conf/nsswitchHandler.py
+++ task3 - nsswitch.conf/nsswitchHandler.py
@@ -1,1 +1,19 @@
 __author__ = 'django'
+import re
+inputFile = open('input.txt','r')
+outFile = open('output.txt','w')
+for line in inputFile:
+    if line[0:5]=='hosts':
+        line = line.replace('dns', '')
+        N = 0
+        for n in range(6,len(line[6:])):
+            if line[n].isspace():
+                N = n
+            else:
+                break
+        line = line[:N] + 'dns' + line[N:]
+    outFile.write(line)
+
+inputFile.close()
+outFile.close()
+
