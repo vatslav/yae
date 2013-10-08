b4c3d350ed8bebdebdec54e43bfbbf83efb0c179
vatslav <vatslav.startup@yandex.ru>
2013-10-08 21:10:33 +0400

    отладка задания 4 нашел баги, тем не менее пока RE

--- task4 - mtrAnalizator/__init__.py
+++ task4 - mtrAnalizator/__init__.py
@@ -1,1 +1,4 @@
 __author__ = 'django'
+
+
+\s*(?P<n>\d{1})\.[|]{1}--\s+(?P<ip>\W+)\s+(?P<loss>\d{1,2}\.\d+)%\s+(?P<cnt>\d+)\s+(?P<last>\d+\.\d+)\s+(?P<avg>\d+\.\d+)\s+(?P<best>\d+\.\d+)\s+(?P<wrst>\d+\.\d+)\s+(?P<stdev>\d+\.\d+).*
--- task4 - mtrAnalizator/mtrAnalisys.py
+++ task4 - mtrAnalizator/mtrAnalisys.py
@@ -14,18 +14,18 @@ class MtrStruct(object):
         self.best=float(best)
         self.wrst=float(wrst)
         self.stdev=float(stdev)
+    def ToString(self):
+        return '{0}, {1}, {2}, {3}'.format(self.n,self.loss,self.stdev, self.avg)
 
-#HOST: pooh                        Loss%   Snt   Last   Avg  Best  Wrst StDev
+#HOST: pooh                       Loss%   Snt   Last   Avg  Best  Wrst StDev
 #  1.|-- 84.201.169.254             0.0%     9    1.3   1.3   1.0   2.4   0.5
 #  2.|-- 37.9.74.134                0.0%     9    1.6  18.4   1.2 121.4  39.8
-#(?P<n>\d{1})\.[|]{1}--\s+(?P<ip>(\d{1,3}\.){3}\d{1,3})\s+(?P<loss>\d{1,2}\.\d+%)\s+(?P<cnt>\d+)\s+(?P<last>\d+\.\d+)\s+(?P<avg>\d+\.\d+)\s+(?P<best>\d+\.\d+)\s+(?P<wrst>\d+\.\d+)\s+(?P<stdev>\d+\.\d+)
 class MtrHandler(object):
     @staticmethod
     def rawDataHandler(mtr):
         mtrStorage = []
-        template = re.compile(r'\s*(?P<n>\d{1})\.[|]{1}--\s+(?P<ip>(\d{1,3}\.){3}\d{1,3})\s+(?P<loss>\d{1,2}\.\d+)%\s+(?P<snt>\d+)\s+(?P<last>\d+\.\d+)\s+(?P<avg>\d+\.\d+)\s+(?P<best>\d+\.\d+)\s+(?P<wrst>\d+\.\d+)\s+(?P<stdev>\d+\.\d+).*')
+        template = re.compile(r'\s*(?P<n>\d{1})\.[|]{1}--\s+(?P<ip>\S+)\s+(?P<loss>\d{1,3}\.\d+)%?\s+(?P<snt>\d+)\s+(?P<last>\d+\.\d+)\s+(?P<avg>\d+\.\d+)\s+(?P<best>\d+\.\d+)\s+(?P<wrst>\d+\.\d+)\s+(?P<stdev>\d+\.\d+).*')
         for line in mtr:
-            print(line)
             m = re.match(template,line)
             if m!=None:
                 mtrStorage.append(MtrStruct(m.group('n'),m.group('ip'),m.group('loss'),m.group('snt'),m.group('last'),m.group('avg'),m.group('best'),m.group('wrst'),m.group('stdev')))
@@ -34,14 +34,29 @@ class MtrHandler(object):
 
     @staticmethod
     def analisys(mtr):
-        storage = MtrHandler.rawDataHandler(mtr)
-        value = storage[0].loss
-        n = 0
-        for i in range(len(storage)):
-            if storage[i].loss<value:
-                value = storage[i].loss
-                n = i
+        def findMax(storage, fieldName):
+            field = lambda f:exec(f)
+            value = eval('storage[0].{0}'.format(fieldName))
+            n = 0
+            allEquals = True
+            for i in range(len(storage)):
+                temp = eval('storage[i].{0}'.format(fieldName))
+                if temp>value:
+                    value = eval('storage[i].{0}'.format(fieldName))
+                    n = storage[i].n
+                    allEquals = False
 
+
+            return (value,n, allEquals)
+
+        storage = MtrHandler.rawDataHandler(mtr)
+        n=0
+        allEquals = True
+        value, n, allEquals = findMax(storage,'loss')
+        if (allEquals):
+            value, n, allEquals = findMax(storage,'stdev')
+            if (allEquals):
+                value, n, allEquals = findMax(storage,'avg')
         return n
 
 
@@ -51,10 +66,7 @@ class IOManager(object):
         outfile = open('output.txt', "w")
         mtr = [line.rstrip() for line in inputfile]
         out = MtrHandler.analisys(mtr)
-
-        print(out)
         outfile.write(str(out))
-
         inputfile.close()
         outfile.close()
 
